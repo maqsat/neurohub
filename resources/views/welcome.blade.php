4f178b0c8cc91b18a146676802786919fb40bab8
Maksat <batyrqul.maqsat@gmail.com>
2024-05-24 23:29:19 +0500

    add yandex metrik and widget

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -501,6 +501,7 @@
     </div>
 </footer>
 
+
 <script src="./assets/js/plugins/lightgallery/lg-thumbnail.min.js"></script>
 <script src="./assets/js/plugins/lightgallery/lg-zoom.min.js"></script>
 <script src="./assets/js/plugins/lightgallery/lightgallery.min.js"></script>
@@ -510,5 +511,28 @@
 <script src="./assets/js/plugins/imask/imask.min.js"></script>
 <script src="./assets/js/plugins/notify/notify.min.js"></script>
 <script src="./assets/js/main.js"></script>
+
+
+<script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script>
+<div class="elfsight-app-7d74b113-21d0-4c28-b7cd-45b2740bb06d" data-elfsight-app-lazy></div>
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(97385880, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+    });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/97385880" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
 </body>
 </html>
