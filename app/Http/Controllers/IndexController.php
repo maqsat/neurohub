c7620d5f936d376d12b7a92356f5d0f9fc6e4589
Maksat <batyrqul.maqsat@gmail.com>
2024-07-04 11:44:53 +0500

    git error

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use App\Models\Post;
+use App\Models\QuestionAnswer;
+use App\Models\Service;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function welcome()
+    {
+        $methods = Post::orderBy('sort')->where('type',1)->take(3)->get();
+        $question_answers = QuestionAnswer::orderBy('sort')->take(6)->get();
+        $reasons = Post::orderBy('sort')->where('type',2)->get();
+        $doctors = User::orderBy('sort')->where('type',1)->get();
+        return view('welcome', compact('methods','reasons','question_answers','doctors'));
+    }
+
+    public function landing()
+    {
+        $methods = Post::orderBy('sort')->where('type',1)->get();
+        $reasons = Post::orderBy('sort')->where('type',2)->take(6)->get();
+        $services = Service::orderBy('sort')->get();
+
+        return view('landing', compact('methods','reasons','services'));
+
+    }
+
+    public function allMethods()
+    {
+        $methods = Post::orderBy('sort')->where('type',1)->get();
+        return view('index.all-methods', compact('methods'));
+    }
+
+    public function showMethods($slug)
+    {
+        $method = Post::where('url',$slug)->first();
+        return view('post.show', compact('method'));
+    }
+
+    public function addOrder(Request $request)
+    {
+
+        $request->validate([
+            'phone' => 'required',
+            'name' => 'required',
+        ]);
+
+
+        Order::create([
+            'phone' => $request->phone,
+            'name' => $request->name,
+            'text' => $request->text,
+        ]);
+
+
+        return view('thank_you_page');
+
+    }
+
+    public function sitemap()
+    {
+        $methods = Post::orderBy('sort')->where('type',1)->get();
+        return  response()->view('sitemap',compact('methods'))->header('Content-Type', 'text/xml');
+    }
+
+}
---
+++ app/Http/Controllers/ServiceController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Service;
+use Illuminate\Http\Request;
+
+class ServiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $list = Service::paginate(30);
+        return view('service.index', compact('list'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('service.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'cost' => 'required',
+            'sort' => 'required',
+        ]);
+
+
+        Service::create([
+            'title' => $request->title,
+            'cost' => $request->cost,
+            'sort' => $request->sort,
+        ]);
+
+
+        return redirect('/services')->with('status', 'Успешно добавлено');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Service  $service
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Service $service)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Service  $service
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Service $service)
+    {
+        return view('service.edit', compact('service'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Service  $service
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Service $service)
+    {
+        $request->validate([
+            'title' => 'required',
+            'cost' => 'required',
+            'sort' => 'required',
+        ]);
+
+
+        $service->update([
+            'title' => $request->title,
+            'cost' => $request->cost,
+            'sort' => $request->sort,
+        ]);
+
+
+        return redirect('/services')->with('status', 'Успешно добавлено');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Service  $service
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Service $service)
+    {
+        //
+    }
+}
